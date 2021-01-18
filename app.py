b522ee7a0806829ebeede543f16d7a28c4ff99c0
lotixdsmr <christopher.chwolka@gmail.com>
2021-01-18 21:08:22 +0100

    Add edit post function

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, flash, redirect
 from werkzeug.exceptions import abort
 
 def get_db_connection():
@@ -18,6 +18,7 @@ def get_post(post_id):
 	return post
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "notverysecretmrgithub"
 
 @app.route('/')
 def index():
@@ -30,3 +31,43 @@ def index():
 def post(post_id):
 	post = get_post(post_id)
 	return render_template('post.html', post=post)
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+	if request.method == 'POST':
+		title = request.form['title']
+		content = request.form['content']
+
+		if not title:
+			flash('Title is required')
+		else:
+			conn = get_db_connection()
+			conn.execute('INSERT INTO posts (title, content) VALUES(?, ?)',
+				(title, content))
+			conn.commit()
+			conn.close()
+			return redirect(url_for('index'))
+
+	return render_template('create.html')
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+	post = get_post(id)
+
+	if request.method == 'POST':
+		title = request.form['title']
+		content = request.form['content']
+
+		if not title:
+			flash('Title is required!')
+		else:
+			conn = get_db_connection()
+			conn.execute('UPDATE posts SET title = ?, content = ?'
+						 ' WHERE id = ?',
+						 title, content, id)
+			conn.commit()
+			conn.close()
+
+		return redirect(url_for('index'))							 	
+
+	render_template('edit.html', post=post)	
